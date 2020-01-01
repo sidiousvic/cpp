fad71787d07fb0ce98f537da8a0c9c99025f2933
sidiousvic <victor6rene@gmail.com>
2020-01-01 11:26:13 +0900

    Initial Commit

---
+++ C++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++
+//
+//  Created by VIC SIDIOUS on 2020/01/01.
+//  Copyright © 2020 SIDIOUSVIC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
