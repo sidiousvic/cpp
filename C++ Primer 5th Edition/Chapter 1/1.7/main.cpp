962d74d93a3eaea365199a4e628fea5f0ee36ef4
sidiousvic <victor6rene@gmail.com>
2020-01-05 13:20:13 +0900

    Exercise 1.7

---
+++ C++ Primer 5th Edition/Chapter 1/1.7/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  1.7
+//
+//  Created by VIC SIDIOUS on 2020/01/04.
+//  Copyright © 2020 SIDIOUSVIC. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    /*
+     * comment pairs /* */ cannot nest.
+    * ''cannot nest'' is considered source code,
+    * as is the rest of the program
+    */
+    std::cout << "Hello, World!\n";
+    return 0;
+}
