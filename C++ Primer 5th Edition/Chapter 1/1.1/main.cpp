c23ced7b75dcbc0f9c08304def8676250b906959
sidiousvic <victor6rene@gmail.com>
2020-01-05 13:19:20 +0900

    cleanup

--- C++ Primer 5th Edition/Chapter 1/1.1/main.cpp
+++ C++ Primer 5th Edition/Chapter 1/1.1/main.cpp
@@ -5,9 +5,15 @@
 //  Created by VIC SIDIOUS on 2020/01/01.
 //  Copyright © 2020 SIDIOUSVIC. All rights reserved.
 //
+//Exercise 1.1: Review the documentation for your compiler and determine
+//what file naming convention it uses. Compile and run the main program from
+//page 2.
 
 #include <iostream>
 
 int main() {
     return 0;
 }
+
+
+
