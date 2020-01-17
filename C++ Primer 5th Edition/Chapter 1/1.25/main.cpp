ffcb4090f4b4e23f84144bb2b25d317de931e0a9
sidiousvic <victor6rene@gmail.com>
2020-01-17 21:14:34 +0900

    1.25

---
+++ C++ Primer 5th Edition/Chapter 1/1.25/main.cpp
@@ -0,0 +1,34 @@
+//
+//  main.cpp
+//  1.25
+//
+//  Created by VIC SIDIOUS on 2020/01/14.
+//  Copyright © 2020 SIDIOUSVIC. All rights reserved.
+//
+
+#include <iostream>
+#include "Sales_item.h"
+int main()
+{
+    Sales_item total; // variable to hold data for the next transaction
+    // read the first transaction and ensure that there are data to process
+    if (std::cin >> total) {
+        Sales_item trans; // variable to hold the running sum
+        // read and process the remaining transactions
+        while (std::cin >> trans) {
+            // if we're still processing the same book
+            if (total.isbn() == trans.isbn())
+                total += trans; // update the running total
+            else {
+                // print results for the previous book
+                std::cout << total << std::endl;
+                total = trans; // total now refers to the next book
+            } }
+        std::cout << total << std::endl; // print the last transaction
+    } else {
+        // no input! warn the user
+        std::cerr << "No data?!" << std::endl;
+        return -1; // indicate failure
+    }
+    return 0;
+}
